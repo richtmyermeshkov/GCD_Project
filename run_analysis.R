41434f1343307374dd38ecd36618c0dcb2242d42
richtmyermeshkov <richtmyermeshkov@gmail.com>
2014-07-27 15:09:00 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 
-#  The following R script file performs the following steps:
+#  The following R script performs the following steps:
 #  1. Merges the training and the test sets to create one data set.
 #  2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 #  3. Uses descriptive activity names to name the activities in the data set
